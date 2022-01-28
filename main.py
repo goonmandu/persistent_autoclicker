c07823cafc446853bb6188ca78298922fa7bf4ab
goonmandu <61984863+goonmandu@users.noreply.github.com>
2022-01-28 01:26:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+from pynput.mouse import Button, Controller
+import keyboard
+
+mouse = Controller()
+STOP_KEY = "alt"
+CPS = 100
+DELAY = 5
+
+for i in range(DELAY):
+    print(f"{DELAY - i} ", end="")
+    time.sleep(1)
+print(f"\nPress [{STOP_KEY.upper()}] to end script.")
+
+while not keyboard.is_pressed("alt"):
+    mouse.press(Button.left)
+    time.sleep(1 / (CPS*2))
+    mouse.release(Button.left)
+    time.sleep(1 / (CPS*2))
